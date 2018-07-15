85cc35a8bb3a2ff436f36a86eb5f7469f3eaf3ee
moerbeke <aceballos@gmail.com>
2018-07-15 05:50:28 +0200

    Version 0.3.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='simcore',
-      version='0.1.0-e',
+      version='0.3.0',
       description='Simcore',
       author='Antonio Ceballos Roa',
       author_email='aceballos@gmail.com',
-      packages=['simcore',],
+      packages=['simcore', 'test'],
       license='GPLv3+',
      )
