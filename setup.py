8f2c6e190644231cbe53ae0f51b2bc2a1102347e
moerbeke <aceballos@gmail.com>
2018-07-25 07:14:51 +0200

    Version 0.3.0-e

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='simcore',
-      version='0.3.0',
+      version='0.3.0-e',
       description='Simcore',
       author='Antonio Ceballos Roa',
       author_email='aceballos@gmail.com',
